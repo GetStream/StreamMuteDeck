d9c68c0f8de9348be9c6798baddd6f706f921fe2
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2024-06-17 15:46:44 +0200

    Make muting of video and audio work.

--- observers/streamVideoObserver.js
+++ observers/streamVideoObserver.js
@@ -1,4 +1,3 @@
-
 class StreamVideoObserver {
   constructor() {
     this._timer = null;
@@ -10,16 +9,18 @@ class StreamVideoObserver {
     this.isVideoStarted = false;
     this.isShareStarted = false;
     this.isRecordStarted = false;
-  };
+  }
 
   initialize = () => {
     // detect whether getstream.io is the hostname of the page, otherwise do not initialize
-    if (window.location.hostname !== 'getstream.io') {
-      console.log('Not on Stream page page, not initializing StreamVideoObserver');
+    if (window.location.hostname !== "getstream.io") {
+      console.log(
+        "Not on Stream page page, not initializing StreamVideoObserver"
+      );
       return;
     }
 
-    console.log('Initializing StreamVideoObserver');
+    console.log("Initializing StreamVideoObserver");
     this._observer = new MutationObserver(this._handleElementChange);
     // listen for SVG class changes
     this._observer.observe(document.body, {
@@ -37,20 +38,20 @@ class StreamVideoObserver {
     this.updateStreamVideoStatus();
   };
 
-
   updateStreamVideoStatus = () => {
     //console.log('Updating Stream Video status');
     let changed = false;
 
     // use the leave button to see if we're in a meeting
-    let callDuration = document.querySelector('div[id="call-duration-custom"]');
+    let callDuration = document.querySelector(
+      'div[class="rd__header__elapsed-time"]'
+    );
     if (callDuration) {
       if (!this.isInMeeting) {
         changed = true;
       }
       this.isInMeeting = true;
-    }
-    else {
+    } else {
       if (this.isInMeeting) {
         changed = true;
       }
@@ -58,52 +59,48 @@ class StreamVideoObserver {
     }
 
     if (this.isInMeeting) {
-
       // find the video button
-      let buttonVideo = document.querySelector('button[id="video-button"]');
-      if (buttonVideo) {
-        let videoStopped = Boolean(buttonVideo.getAttribute("data-state") === 'call-video-off');
-        if (videoStopped) {
-          if (this.isVideoStarted) {
-            changed = true;
-          }
-          this.isVideoStarted = false;
+      let buttonVideoUnmuted = document.querySelector(
+        'button[data-testid="video-unmute-button"]'
+      );
+      if (buttonVideoUnmuted) {
+        if (this.isVideoStarted) {
+          changed = true;
         }
-        else {
-          if (!this.isVideoStarted) {
-            changed = true;
-          }
-          this.isVideoStarted = true;
+        this.isVideoStarted = false;
+      } else {
+        if (!this.isVideoStarted) {
+          changed = true;
         }
+        this.isVideoStarted = true;
       }
 
       // find the mute button
-      let buttonMic = document.querySelector('button[id="microphone-button"]');
-      if (buttonMic) {
-        let muted = Boolean(buttonMic.getAttribute("data-state") === 'mic-off');
-        if (muted) {
-          if (!this.isMuted) {
-            changed = true;
-          }
-          this.isMuted = true;
+      let buttonMicUnmuted = document.querySelector(
+        'button[data-testid="audio-unmute-button"]'
+      );
+      if (buttonMicUnmuted) {
+        if (!this.isMuted) {
+          changed = true;
         }
-        else {
-          if (!this.isMuted) {
-            changed = true;
-          }
-          this.isMuted = false;
+        this.isMuted = true;
+      } else {
+        if (!this.isMuted) {
+          changed = true;
         }
+        this.isMuted = false;
       }
 
       // find the recording indicator
-      let recordingIndicator = document.querySelector('div[id="recording-indicator-custom"]');
+      let recordingIndicator = document.querySelector(
+        'div[id="recording-indicator-custom"]'
+      );
       if (recordingIndicator) {
         if (!this.isRecordStarted) {
           changed = true;
         }
         this.isRecordStarted = true;
-      }
-      else {
+      } else {
         if (this.isRecordStarted) {
           changed = true;
         }
@@ -113,21 +110,22 @@ class StreamVideoObserver {
       // find the sharing button
       let buttonShare = document.querySelector('button[id="share-button"]');
       if (buttonShare) {
-        let presenting = Boolean(buttonShare.getAttribute("data-state") === 'call-control-stop-presenting-new');
+        let presenting = Boolean(
+          buttonShare.getAttribute("data-state") ===
+            "call-control-stop-presenting-new"
+        );
         if (presenting) {
           if (!this.isShareStarted) {
             changed = true;
           }
           this.isShareStarted = true;
         }
-        else {
-          if (this.isShareStarted) {
-            changed = true;
-          }
-          this.isShareStarted = false;
+      } else {
+        if (this.isShareStarted) {
+          changed = true;
         }
+        this.isShareStarted = false;
       }
-
     } // end if this.isInMeeting
 
     // send meeting status if it has been updated, or if it's been 1 second (250ms * 4) since the last update
@@ -137,22 +135,43 @@ class StreamVideoObserver {
     } else {
       this._updateLoops++;
     }
-  }
-
+  };
 
   /**
    * Actions
    */
 
   toggleMute = () => {
-    let buttonMic = document.querySelector('button[id="microphone-button"]');
-    buttonMic.click();
-  }
+    let micUnmuteButton = document.querySelector(
+      "button[data-testid='audio-unmute-button']"
+    );
+    if (micUnmuteButton) {
+      micUnmuteButton.click();
+    } else {
+      let micMuteButton = document.querySelector(
+        "button[data-testid='audio-mute-button']"
+      );
+      if (micMuteButton) {
+        micMuteButton.click();
+      }
+    }
+  };
 
   toggleVideo = () => {
-    let buttonVideo = document.querySelector('button[id="video-button"]');
-    buttonVideo.click();
-  }
+    let videoUnmuteButton = document.querySelector(
+      "button[data-testid='video-unmute-button']"
+    );
+    if (videoUnmuteButton) {
+      videoUnmuteButton.click();
+    } else {
+      let videoMuteButton = document.querySelector(
+        "button[data-testid='video-mute-button']"
+      );
+      if (videoMuteButton) {
+        videoMuteButton.click();
+      }
+    }
+  };
 
   toggleShare = () => {
     let buttonShare = document.querySelector('button[id="share-button"]');
@@ -160,13 +179,13 @@ class StreamVideoObserver {
   };
 
   toggleRecord = () => {
-    console.log('Recording is not supported via web client');
+    console.log("Recording is not supported via web client");
   };
 
   leaveCall = () => {
     let buttonLeave = document.querySelector('button[id="hangup-button"]');
     buttonLeave.click();
-  }
+  };
 
   _pressPossibleConfirmationButton = () => {
     // Get the iframe element
@@ -175,28 +194,33 @@ class StreamVideoObserver {
       return;
     }
 
-    let confirmationButton = iframeDocument.querySelector('button.leave-meeting-options__btn');
+    let confirmationButton = iframeDocument.querySelector(
+      "button.leave-meeting-options__btn"
+    );
     if (confirmationButton) {
-      console.log('Clicking confirmation button');
+      console.log("Clicking confirmation button");
       confirmationButton.click();
     }
-  }
+  };
 
   sendStreamVideoStatus = () => {
     if (!this.isInMeeting) {
       return;
     }
     const message = {
-      'source': 'browser-extension-plugin',
-      'action': 'update-status',
-      'status': this.isInMeeting ? 'call' : 'closed',
-      'mute': this.isMuted ? 'muted' : 'unmuted',
-      'video': this.isVideoStarted ? 'started' : 'stopped',
-      'share': this.isShareStarted ? 'started' : 'stopped',
-      'record': this.isRecordStarted ? 'started' : 'stopped',
-      'control': 'teams-web',
+      source: "browser-extension-plugin",
+      action: "update-status",
+      status: this.isInMeeting ? "call" : "closed",
+      mute: this.isMuted ? "muted" : "unmuted",
+      video: this.isVideoStarted ? "started" : "stopped",
+      share: this.isShareStarted ? "started" : "stopped",
+      record: this.isRecordStarted ? "started" : "stopped",
+      custom_svg_path: ":/images/logo.svg",
     };
     console.log(message);
-    chrome.runtime.sendMessage({ action: "updateMuteDeckStatus", message: message });
-  }
+    chrome.runtime.sendMessage({
+      action: "updateMuteDeckStatus",
+      message: message,
+    });
+  };
 }
